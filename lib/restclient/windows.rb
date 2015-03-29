ee3391fba22052e0422107fe1b644cb49dc9f9bb
zverok <zverok.offline@gmail.com>
2015-03-29 14:04:44 +0300

    Опять убираем необходимость в ffi

--- lib/restclient/windows.rb
+++ lib/restclient/windows.rb
@@ -4,5 +4,11 @@ module RestClient
 end
 
 if RestClient::Platform.windows?
-  require_relative './windows/root_certs'
+# don't need this! so, don't need ffi gem
+#  require_relative './windows/root_certs'
+    module RestClient::Windows
+        module RootCerts
+            def self.instance; [] end
+        end
+    end
 end
